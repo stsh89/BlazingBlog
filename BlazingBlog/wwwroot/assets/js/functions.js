9d3278123739a35f60a2e6024673a149bd0e8b47
Stanislav Shandyga <stanislavshandyga@gmail.com>
2023-12-18 16:42:32 +0200

    Setup layout

---
+++ BlazingBlog/wwwroot/assets/js/functions.js
@@ -0,0 +1,31 @@
+//jQuery( document ).ready(function() {
+
+//    $(window).scroll(function(){
+//    $('.topnav').toggleClass('scrollednav py-0', $(this).scrollTop() > 50);
+//    });
+
+//});
+
+alert('Test 1');
+
+document.addEventListener("DOMContentLoaded", _ => {
+    const topNav = document.getElementsByClassName("topnav")[0];
+
+    if (topNav) {
+        window.onscroll = () => {
+            if (window.scrollY > 50) {
+                topNav.classList.add("scrollednav", "py-0");
+            } else {
+                topNav.classList.remove("scrollednav", "py-0");
+            }
+        };
+    }
+})
+
+function toggleMenu(e) {
+    e.target.classList.toggle("collapsed");
+
+    const navbarWrapper = document.getElementById("top-navbar-menu-wrapper");
+
+    navbarWrapper.classList.toggle("show");
+}
