64ae9a5cf0b17e9e882b1b202a55648cc403ff65
Stanislav Shandyga <stanislavshandyga@gmail.com>
2023-12-20 17:05:59 +0200

    Add name to application user

--- BlazingBlog/Data/ApplicationUser.cs
+++ BlazingBlog/Data/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace BlazingBlog.Data
@@ -5,6 +6,8 @@ namespace BlazingBlog.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [Required, MaxLength(25)]
+        public string Name { get; set; }
     }
 
 }
